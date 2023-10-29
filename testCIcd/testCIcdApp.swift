fc0a997385bce1987c88dc63ed8c15103aa0e7db
bryangarcia <bryan.checkoapp@gmail.com>
2023-10-29 10:19:59 -0600

    Initial Commit

---
+++ testCIcd/testCIcdApp.swift
@@ -0,0 +1,17 @@
+//
+//  testCIcdApp.swift
+//  testCIcd
+//
+//  Created by bryangarcia on 29/10/23.
+//
+
+import SwiftUI
+
+@main
+struct testCIcdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
